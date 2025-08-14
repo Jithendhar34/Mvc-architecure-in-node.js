8aa1582f2165c8d1b49ce8fd712245e12e50f213
KJithendhar <jithendharkampati04@gmail.com>
2025-08-14 06:38:00 +0530

    one

--- ProductService/config/db.js
+++ ProductService/config/db.js
@@ -9,7 +9,9 @@ const connectDB = async () => {
   } catch (error) {
     console.error("Database connection failed:", error);
     process.exit(1);
-  }
+  }//commit 
+  //one
+  //two
 };
 
 module.exports = connectDB;
